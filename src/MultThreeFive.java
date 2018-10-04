327b3a1231f01e910cb73659c2c01b8bc1a469d7
Matthew Morgan <mmorgan55@cnm.edu>
2018-10-04 08:07:19 -0600

    Reformatted code and made mult35 method private

--- src/MultThreeFive.java
+++ src/MultThreeFive.java
@@ -1,16 +1,16 @@
 public class MultThreeFive {
+
   public static void main(String[] args) {
     System.out.println(mult35(1000));
   }
 
-  public static int mult35(int limit) {
+  private static int mult35(int limit) {
     int total = 0;
-    for (int i =0; i < limit; i++) {
-      if (i%3 == 0 || i%5 ==0) {
-        total+=i;
+    for (int i = 0; i < limit; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        total += i;
       }
     }
-
     return total;
   }
 }
